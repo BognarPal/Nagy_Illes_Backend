9cf0436be1b86635d19201073a811f6333fa02b9
KondorArminAron23 <kondor.armin.aron@students.jedlik.eu>
2023-01-04 15:11:16 +0100

    Created UserModel.

---
+++ Discite.Data/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discite.Data.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Hash { get; set; }
+
+        public string Salt { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+
+    }
+}
