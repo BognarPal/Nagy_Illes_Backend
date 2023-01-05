1a31c76c6bfe23eeb60d2b098f89c384ec10b746
NagyIlles03 <nagy.illes@students.jedlik.eu>
2023-01-05 16:03:27 +0100

    Implemented login function

--- Discite.API/Controllers/UsersController.cs
+++ Discite.API/Controllers/UsersController.cs
@@ -1,5 +1,4 @@
-﻿
-using Discite.Data;
+﻿using Discite.Data;
 using Discite.Data.Models;
 using Discite.Data.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -8,7 +7,6 @@ using System.Text;
 
 namespace Discite.API.Controllers
 {
-    
     public class UsersController : BaseApiController
     {
         UserRepository userRepository;
@@ -24,15 +22,13 @@ namespace Discite.API.Controllers
         }
 
         [HttpGet("{id}")]
-
         public UserModel GetUsers(int id)
         {
             return userRepository.GetAll().SingleOrDefault(U => U.Id == id);
         }
 
         [HttpPost("register")]
-
-        public async Task<ActionResult<UserModel>> Register (string username, string email, string password)
+        public async Task<ActionResult<UserModel>> Register(string username, string email, string password)
         {
             using var hmac = new HMACSHA256();
 
@@ -48,7 +44,19 @@ namespace Discite.API.Controllers
 
             return user;
         }
-        
 
+        [HttpPost("login")]
+        public async Task<ActionResult<UserModel>> Login(string email, string password)
+        {
+            UserModel user = userRepository.GetAll().SingleOrDefault(u => u.Email == email);
+
+            if (user == null) return Unauthorized("Invalid username or password");
+
+            using var hmac = new HMACSHA256(user.Salt);
+
+            if (hmac.ComputeHash(Encoding.UTF8.GetBytes(password)) != user.Hash) return Unauthorized("Invalid username or password");
+
+            return user;
+        } 
     }
 }
