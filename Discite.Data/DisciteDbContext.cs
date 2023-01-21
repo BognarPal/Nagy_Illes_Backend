88e65251268b2e666d92c728c47bad815e8633d6
NagyIlles03 <nagy.illes@students.jedlik.eu>
2023-01-21 14:51:22 +0100

    Filled ORM DB context with fix game datas

--- Discite.Data/DisciteDbContext.cs
+++ Discite.Data/DisciteDbContext.cs
@@ -28,7 +28,53 @@ namespace Discite.Data
                 optionsBuilder.UseMySql(connString, ServerVersion.AutoDetect(connString));
         }
 
-        public DbSet<UserModel> Users { get; set; }
-       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserModel>().HasIndex(e => e.Email).IsUnique();
+
+            modelBuilder.Entity<EnemyModel>().HasData
+            (
+                new EnemyModel() { Id = 1, Name = "Ghoul", MaxHp = 20, Damage = 10, Energy = 0, Speed = 0.7f },
+                new EnemyModel() { Id = 2, Name = "Exploder", MaxHp = 5, Damage = 25, Energy = 0, Speed = 1 },
+                new EnemyModel() { Id = 3, Name = "Cyber Ghoul", MaxHp = 35, Damage = 20, Energy = 0, Speed = 1.5f },
+                new EnemyModel() { Id = 4, Name = "Multi-tank", MaxHp = 60, Damage = 10, Energy = 0, Speed = 0.5f },
+                new EnemyModel() { Id = 5, Name = "Agent", MaxHp = 20, Damage = 15, Energy = 0, Speed = 0.7f },
+                new EnemyModel() { Id = 6, Name = "Chimera", MaxHp = 200, Damage = 15, Energy = 0, Speed = 1.2f }
+            );
+
+            modelBuilder.Entity<ClassModel>().HasData
+            (
+                new ClassModel() { Id = 1, Name = "Artificier", MaxHp = 70, Damage = 5, Energy = 160, Speed = 1 },
+                new ClassModel() { Id = 2, Name = "Weapon Master", MaxHp = 120, Damage = 4, Energy = 100, Speed = 1 },
+                new ClassModel() { Id = 3, Name = "Cyber Ninja", MaxHp = 90, Damage = 4, Energy = 130, Speed = 2 }
+            );
+
+            modelBuilder.Entity<WeaponModel>().HasData
+            (
+                new WeaponModel() { Id = 1, Name = "Katana", Damage = 3, AttackSpeed = 1.5f },
+                new WeaponModel() { Id = 2, Name = "Spear", Damage = 4, AttackSpeed = 1 },
+                new WeaponModel() { Id = 3, Name = "Deagle", Damage = 6, AttackSpeed = 0.7f },
+                new WeaponModel() { Id = 4, Name = "Laser SMG", Damage = 1, AttackSpeed = 3 },
+                new WeaponModel() { Id = 5, Name = "Shotgun", Damage = 1, AttackSpeed = 0.5f }
+            );
+
+            modelBuilder.Entity<ArtifactModel>().HasData
+            (
+                new ArtifactModel() { Id = 1, Name = "Flammable blood", MaxLevel = 3 },
+                new ArtifactModel() { Id = 2, Name = "Poisonous blood", MaxLevel = 3 },
+                new ArtifactModel() { Id = 3, Name = "Exploding corpses", MaxLevel = 3 },
+                new ArtifactModel() { Id = 4, Name = "Revenge damage", MaxLevel = 3 }
+            );
+
+            modelBuilder.Entity<RoomModel>().HasData
+            (
+                new RoomModel() { Id = 1, Name = "Entry room" },
+                new RoomModel() { Id = 2, Name = "Exit room" },
+                new RoomModel() { Id = 3, Name = "Encounter room" },
+                new RoomModel() { Id = 4, Name = "Shop room" },
+                new RoomModel() { Id = 5, Name = "Boss room" }
+            );
+        }
     }
 }
