f0c933e0ded068278f72735e0fa796be61d8321d
NagyIlles03 <nagy.illes@students.jedlik.eu>
2023-02-12 20:04:53 +0100

    Implemented configuration getter API endpoint

---
+++ Discite.API/Controllers/ConfigurationController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Discite.Data.Repositories;
+using Discite.Data.Models;
+using System.Text.Json;
+
+namespace Discite.API.Controllers
+{
+    public class ConfigurationController : BaseApiController
+    {
+        private class Config
+        {
+            public Config(WeaponRepository weaponRepository, EnemyRepository enemyRepository, ArtifactRepository artifactRepository, ClassRepository classRepository) 
+            {
+                this.Weapons = weaponRepository.GetAll();
+                this.Enemies = enemyRepository.GetAll();
+                this.Artifacts = artifactRepository.GetAll();
+                this.Classes = classRepository.GetAll();
+            }
+            public IEnumerable<WeaponModel> Weapons { get; set; }
+            public IEnumerable<ArtifactModel> Artifacts { get; set; }
+            public IEnumerable<EnemyModel> Enemies { get; set; }
+            public IEnumerable<ClassModel> Classes { get; set; }
+
+        }
+        WeaponRepository weaponRepository;
+        EnemyRepository enemyRepository;
+        ArtifactRepository artifactRepository;
+        ClassRepository classRepository;
+        public ConfigurationController()
+        {
+            weaponRepository = new WeaponRepository();
+            enemyRepository = new EnemyRepository();
+            artifactRepository = new ArtifactRepository();
+            classRepository = new ClassRepository();
+
+        }
+
+        [HttpGet]
+        public string Configuration()
+        {
+            return JsonSerializer.Serialize(new Config(weaponRepository, enemyRepository, artifactRepository, classRepository));
+        }
+    }
+}
