3fc6affde71fbddb9fba0c724ced06a0a45db638
NagyIlles03 <nagy.illes@students.jedlik.eu>
2023-02-13 20:00:22 +0100

    Implemented Runs Controller

---
+++ Discite.API/Controllers/RunsController.cs
@@ -0,0 +1,41 @@
+﻿using Discite.Data.Repositories;
+using Discite.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discite.API.Controllers
+{
+    public class RunsController : BaseApiController
+    {
+        RunRepository runRepository;
+
+        public RunsController(RunRepository runRepository)
+        {
+            this.runRepository = runRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Load(int rid)
+        {
+            var run = runRepository.GetAll().SingleOrDefault(r => r.Id == rid);
+            if (run == null)
+                return NotFound();
+            else
+                return Ok(run);
+        }
+
+        [HttpPost]
+        public IActionResult Save([FromBody]RunModel run)
+        {
+            runRepository.Update(run);
+            return Ok();
+        }
+
+        [HttpPost("new")]
+        public IActionResult NewGame() 
+        {
+            RunModel run = new RunModel();
+            var crun = runRepository.Insert(run);
+            return Created($"api/Runs/{crun.Id}", crun);
+        }
+    }
+}
