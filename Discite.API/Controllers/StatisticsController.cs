fc26be712b5911e0417fab8dbb847d161e425b20
NagyIlles03 <nagy.illes@students.jedlik.eu>
2023-02-16 19:27:32 +0100

    Made player toplist getter endpoint

--- Discite.API/Controllers/StatisticsController.cs
+++ Discite.API/Controllers/StatisticsController.cs
@@ -1,17 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Discite.Data.Models;
+using Discite.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Discite.API.Controllers
 {
     public class StatisticsController : BaseApiController
     {
+        UserRepository userRepository;
+        EnemyRepository enemyRepository;
+        RoomRepository roomRepository;
+        WeaponRepository weaponRepository;
+        RunRepository runRepository;
+        ClassRepository classRepository;
+        ArtifactRepository artifactRepository;
 
         public StatisticsController() 
         {
-
+            userRepository = new UserRepository();
+            enemyRepository = new EnemyRepository();
+            roomRepository = new RoomRepository();
+            weaponRepository = new WeaponRepository();
+            runRepository = new RunRepository();
+            classRepository = new ClassRepository();
+            artifactRepository = new ArtifactRepository();
         }
-        public IActionResult Index()
+        [HttpGet("toplist")]
+        public IEnumerable<UserModel> TopPlayers()
         {
-            return View();
+            return userRepository.GetAll().OrderByDescending(u => u.Runs.Sum(r => r.Score));
         }
     }
 }
