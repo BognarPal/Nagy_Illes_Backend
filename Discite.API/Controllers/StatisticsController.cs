3123e0b3d09a8ab00e6070cc690546ecff25c914
NagyIlles03 <nagy.illes@students.jedlik.eu>
2023-02-16 19:11:20 +0100

    Implemented statistics controller

---
+++ Discite.API/Controllers/StatisticsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Discite.API.Controllers
+{
+    public class StatisticsController : BaseApiController
+    {
+
+        public StatisticsController() 
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
