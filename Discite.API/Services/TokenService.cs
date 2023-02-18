de1a2bcd9bc16bbf0e4d6e322638516a46267e8c
NagyIlles03 <nagy.illes@students.jedlik.eu>
2023-02-18 18:26:30 +0100

    Implemented Token Service

--- Discite.API/Services/TokenService.cs
+++ Discite.API/Services/TokenService.cs
@@ -1,13 +1,42 @@
 ﻿using Discite.API.Interfaces;
 using Discite.Data.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Discite.API.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey key;
+        private readonly IConfiguration config;
+        public TokenService(IConfiguration config) 
+        {
+            this.config = config;
+            key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.config["JWT:Key"]));
+        }
         public string CreateToken(UserModel user)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.Email)
+            };
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor 
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddMinutes(double.Parse(this.config["JWT:ExpireInMinute"])),
+                SigningCredentials = creds,
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
