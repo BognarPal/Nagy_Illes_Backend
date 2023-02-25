0d077437eed7f022afaa6df48713a65de3f00af6
NagyIlles03 <nagy.illes@students.jedlik.eu>
2023-02-25 11:56:13 +0100

    Added DTO validations

--- Discite.API/DTOs/LoginDto.cs
+++ Discite.API/DTOs/LoginDto.cs
@@ -1,8 +1,15 @@
-﻿namespace Discite.API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Discite.API.DTOs
 {
     public class LoginDto
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(32, MinimumLength = 8)]
         public string Password { get; set; }
     }
 }
--- Discite.API/DTOs/RegisterDto.cs
+++ Discite.API/DTOs/RegisterDto.cs
@@ -1,9 +1,19 @@
-﻿namespace Discite.API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Discite.API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
+        [StringLength(32, MinimumLength = 2)]
         public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(32, MinimumLength = 8)]
         public string Password { get; set; }
     }
 }
