8212857e3ba6f959d72dfabeb6866fbe1ceabdf3
NagyIlles03 <nagy.illes@students.jedlik.eu>
2023-02-25 12:44:39 +0100

    Added new DTOs

---
+++ Discite.API/DTOs/ClassStatsDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Discite.API.DTOs
+{
+    public class ClassStat
+    {
+        public int Id { get; set; }
+        public int Owned { get; set; }
+        public int Used { get; set; }
+        public int Kills { get; set; }
+        public int Deaths { get; set; }
+    }
+    public class ClassDto
+    {
+        public ClassStat[] ClassStats { get; set; }
+    }
+}
