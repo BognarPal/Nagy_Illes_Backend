3de7d9fc5e50f26159e34387461bb4934656b63e
NagyIlles03 <nagy.illes@students.jedlik.eu>
2023-02-27 11:53:01 +0100

    Added seed migration

---
+++ Discite.Data/Migrations/20230227105217_Seed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Discite.Data.Migrations
+{
+    public partial class Seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Seed",
+                table: "run",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Seed",
+                table: "run");
+        }
+    }
+}
