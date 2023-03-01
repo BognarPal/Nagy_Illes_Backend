f95e78574c3ebebcf9a9007bed4a8874a9618cc0
NagyIlles03 <nagy.illes@students.jedlik.eu>
2023-03-01 17:54:16 +0100

    Implemented some tests

--- Discite.Test/DisciteApiTest.cs
+++ Discite.Test/DisciteApiTest.cs
@@ -1,4 +1,5 @@
 using Discite.Data.Models;
+using Discite.Data.Repositories;
 using Discite.API.DTOs;
 using RestSharp;
 using System.Net;
@@ -7,12 +8,14 @@ namespace Discite.Test
 {
     public class DisciteApiTest : IDisposable
     {
+        UserRepository userRepository;
         string url = "http://localhost:5100";
         readonly RestClient client;
 
         public DisciteApiTest()
         {
             this.client = new RestClient(url);
+            this.userRepository = new UserRepository();
         }
 
         public void Dispose()
@@ -21,13 +24,45 @@ namespace Discite.Test
         }
 
         [Fact]
-        public void RegisterUser()
+        public void Register()
         {
-            RegisterDto registerDto = new RegisterDto() { Email = "asd@asd.asd", Password = "asdasdasd", Username = "asdasd"};
+            var user = RegisterUser("asd");
 
-            RestRequest request = new RestRequest("/api/users/register", Method.Get);
+        }
 
-            client.Get<UserDto>(request);
+        [Fact]
+        public void EditProfile()
+        {
+
+        }
+
+        private UserDto? Login(string email, string password) 
+        {
+            RestRequest request = new RestRequest("/api/users/login", Method.Post);
+            request.AddJsonBody(new LoginDto { Email = email, Password = password });
+
+            return client.Post<UserDto>(request);
+        }
+
+        private UserDto? RegisterUser(string name)
+        {
+            RegisterDto registerDto = new RegisterDto() { Email = $"{name.ToLower()}@test.test", Password = name, Username = name };
+
+            RestRequest request = new RestRequest("/api/users/register", Method.Post);
+
+            request.AddJsonBody(registerDto);
+
+            return client.Post<UserDto>(request);
+        }
+
+        private UserDto? RegisterAdmin()
+        {
+            var user = RegisterUser("admin");
+            var user2 = userRepository[user.Id];
+            user2.Id = 0;
+            userRepository.Delete(user.Id);
+            userRepository.Insert(user2);
+            return new UserDto { Id= user2.Id, Email = user2.Email, Username = user2.UserName };
         }
     }
 }
