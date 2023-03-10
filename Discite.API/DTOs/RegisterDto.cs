92e3a478357557e6874b7a838b133a8aaa71a3fa
NagyIlles03 <nagy.illes@students.jedlik.eu>
2023-03-10 13:32:08 +0100

    Added validatior error messages for register dto

--- Discite.API/DTOs/RegisterDto.cs
+++ Discite.API/DTOs/RegisterDto.cs
@@ -5,15 +5,15 @@ namespace Discite.API.DTOs
     public class RegisterDto
     {
         [Required]
-        [StringLength(32, MinimumLength = 2)]
+        [StringLength(32, MinimumLength = 2, ErrorMessage = "A felhasználónév minimum 2 és maximum 32 karakter hosszú lehet")]
         public string Username { get; set; }
 
         [Required]
-        [EmailAddress]
+        [EmailAddress(ErrorMessage = "Hibás e-mail cím")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(32, MinimumLength = 8)]
+        [StringLength(32, MinimumLength = 8, ErrorMessage = "A jelszó minimum 8 karakter hosszú lehet")]
         public string Password { get; set; }
     }
 }
