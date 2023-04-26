2c064b51b598caa22727814178fcbf52ac89c6b5
NagyIlles03 <nagy.illes@students.jedlik.eu>
2023-04-26 14:20:43 +0200

    Simplifies

--- Discite.API/DTOs/ConfigurationDto.cs
+++ Discite.API/DTOs/ConfigurationDto.cs
@@ -1,9 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Discite.Data.Models;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Discite.API.DTOs
 {
     public class Weapon
     {
+        public Weapon() { }
+        public Weapon(WeaponModel weapon)
+        {
+            Id = weapon.Id;
+            Name = weapon.Name;
+            Damage = weapon.Damage;
+            Speed = weapon.Speed;
+        }
         public int Id { get; set; }
         public string Name { get; set; }
         public float Damage { get; set; }
@@ -11,6 +20,15 @@ namespace Discite.API.DTOs
     }
     public class Enemy
     {
+        public Enemy() { }
+        public Enemy(EnemyModel enemy)
+        {
+            Id = enemy.Id;
+            Name = enemy.Name;
+            Health = enemy.Health;
+            Damage = enemy.Damage;
+            Speed = enemy.Speed;
+        }
         public int Id { get; set; }
         public string Name { get; set; }
         public float Health { get; set; }
@@ -19,6 +37,13 @@ namespace Discite.API.DTOs
     }
     public class Artifact
     {
+        public Artifact() { }
+        public Artifact(ArtifactModel artifact)
+        {
+            Id = artifact.Id;
+            Name = artifact.Name;
+            Power = artifact.Power;
+        }
         public int Id { get; set; }
         public string Name { get; set; }
         public float Power { get; set; }
